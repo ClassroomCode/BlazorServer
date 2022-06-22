ac360b43bebe181b458dc4721478db33bc966d2d
Jason Bell <jbell@accelebrate.com>
2022-06-22 16:25:13 -0400

    ConnStr

--- EComm/ECommWeb/Program.cs
+++ EComm/ECommWeb/Program.cs
@@ -1,6 +1,7 @@
 using ECommWeb.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContextFactory<ECommContext>(opt =>
+    opt.UseSqlServer(""));
 
 var app = builder.Build();
 

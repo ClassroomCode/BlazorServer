0f787ea2ea23df470c1e1bcb6ce5ad2ce605d583
Jason Bell <jbell@accelebrate.com>
2022-06-22 15:28:49 -0400

    Product and supplier

---
+++ EComm/ECommWeb/Data/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace ECommWeb.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public decimal? UnitPrice { get; set; }
+        public string? Package { get; set; }
+        public bool IsDiscontinued { get; set; }
+
+        public int SupplierId { get; set; }
+        // public Supplier? Supplier { get; set; }
+    }
+}
---
+++ EComm/ECommWeb/Data/Supplier.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommWeb.Data
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; } = string.Empty;
+    }
+}

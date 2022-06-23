225c7ef0817ea79494cec7f83f5a67e0b69e5535
Jason Bell <jbell@accelebrate.com>
2022-06-23 15:37:40 -0400

    Edit p3

--- EComm/ECommWeb/Data/Product.cs
+++ EComm/ECommWeb/Data/Product.cs
@@ -1,10 +1,17 @@
-﻿namespace ECommWeb.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommWeb.Data;
 
 public class Product
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage ="Product must have a name")]
     public string ProductName { get; set; } = string.Empty;
+
+    [Range(1, 5000)]
     public decimal? UnitPrice { get; set; }
+
     public string? Package { get; set; }
     public bool IsDiscontinued { get; set; }
 

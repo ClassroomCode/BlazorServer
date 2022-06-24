31794f8d8d77237e0a107d459ee781d5ebc5b648
Jason Bell <jbell@accelebrate.com>
2022-06-24 12:30:07 -0400

    Split form

--- EComm/ECommWeb/Data/Supplier.cs
+++ EComm/ECommWeb/Data/Supplier.cs
@@ -1,7 +1,11 @@
-﻿namespace ECommWeb.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommWeb.Data;
 
 public class Supplier
 {
     public int Id { get; set; }
+
+    [Required]
     public string CompanyName { get; set; } = string.Empty;
 }

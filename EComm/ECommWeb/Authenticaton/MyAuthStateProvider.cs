2b8a983d44a4393d3bee0aed1ba598f866a6702d
Jason Bell <jbell@accelebrate.com>
2022-06-24 14:52:52 -0400

    Auth part one

---
+++ EComm/ECommWeb/Authenticaton/MyAuthStateProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Security.Claims;
+
+namespace ECommWeb.Authenticaton
+{
+    public class MyAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ProtectedBrowserStorage _sessionStorage;
+        private ClaimsPrincipal _anon = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public MyAuthStateProvider(ProtectedBrowserStorage sessionStorage)
+        {
+            _sessionStorage = sessionStorage;
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
